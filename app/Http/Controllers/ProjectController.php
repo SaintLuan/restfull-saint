9d2cd5d990a9f91caa488148ad964b99a8aeabd6
Luan Santos <50582067+SaintLuan@users.noreply.github.com>
2023-06-03 10:28:27 -0300

    [Release] Auth with JWT

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Orion\Http\Controllers\Controller;
+
+class ProjectController extends Controller{
+    protected $model = Project::class;
+}
---
+++ database/migrations/2023_06_02_225251_create_cases_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('url');
+            $table->string('url_production');
+            $table->string('image');
+            $table->string('description');
+            $table->string('cattegorys');
+            $table->tinyInteger('is_master');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
